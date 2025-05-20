3d803278ec6a8b6b481cf1efb25850eec444c565
Sujan Banjara <banjara.suzan12@gmail.com>
2025-05-20 15:22:12 -0500

    Add getValue method to Portfolio to calculate total value of assets

--- Portfolio/src/main/java/com/pluralsight/finance/Portfolio.java
+++ Portfolio/src/main/java/com/pluralsight/finance/Portfolio.java
@@ -17,4 +17,12 @@ public class Portfolio {
     public void add(Valuable asset) {
         assets.add(asset);
     }
+
+    public double getValue() {
+        double total = 0;
+        for (Valuable asset : assets) {
+            total += asset.getValue();
+        }
+        return total;
+    }
 }
