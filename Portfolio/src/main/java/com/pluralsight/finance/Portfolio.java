96680e653fe746b8289283b7a24b6167779c16aa
Sujan Banjara <banjara.suzan12@gmail.com>
2025-05-20 15:20:43 -0500

    Create Portfolio class with name, owner, and add method

---
+++ Portfolio/src/main/java/com/pluralsight/finance/Portfolio.java
@@ -0,0 +1,20 @@
+package com.pluralsight.finance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Portfolio {
+    private String name;
+    private String owner;
+    private List<Valuable> assets;
+
+    public Portfolio(String name, String owner) {
+        this.name = name;
+        this.owner = owner;
+        this.assets = new ArrayList<>();
+    }
+
+    public void add(Valuable asset) {
+        assets.add(asset);
+    }
+}
